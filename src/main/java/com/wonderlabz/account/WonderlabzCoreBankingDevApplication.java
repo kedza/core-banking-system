68e79e3dc8b146c3c3fb2fd5b8907971f8d01f8d
kgoredema <kgoredema@bancabc.co.zw>
2021-07-19 06:42:26 +0200

    project setup

---
+++ src/main/java/com/wonderlabz/account/WonderlabzCoreBankingDevApplication.java
@@ -0,0 +1,13 @@
+package com.wonderlabz.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderlabzCoreBankingDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WonderlabzCoreBankingDevApplication.class, args);
+    }
+
+}
